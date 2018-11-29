f41273ca69756a7fab95a9cc6f6ee23b1190fec2
reymesson1 <rey_messon@hotmail.com>
2018-11-29 15:07:55 -0400

    1

--- webPaa1047/test3.js
+++ webPaa1047/test3.js
@@ -8,12 +8,3 @@ var By = webdriver.By;
 
 var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
 
-var promise = require('selenium-webdriver').promise;
-
-var dataMaster = {}
-
-driver.get('https://www.corotos.com.do/profile');
-
-driver.findElement(By.id('pid_email')).sendKeys('reymesson@gmail.com')
-driver.findElement(By.className('_2QhqT _3NHOV _3z-oQ _2El2O _7xPBo _2pXQq _3WoTQ gO5qu _2h31L _1Dh7N')).click()
-
